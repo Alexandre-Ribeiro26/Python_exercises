8a2ca5bd757711f4b88e689a8f581c075ca41bfe
Alexandre Ribeiro <53876467+Alexandre-Ribeiro26@users.noreply.github.com>
2021-08-03 00:54:22 -0300

    Create Exercise_7.py

---
+++ The Python Workbook (Ben Stepheson)/Exercise_7.py
@@ -0,0 +1,7 @@
+# Sum of the First n Positive Integers
+
+number = int(input('Please, give me a integer and posite number!'))
+
+sum = int((number * (number +1))/ 2)
+
+print(f'The sum of the fist {number} positive integers is {sum}.')

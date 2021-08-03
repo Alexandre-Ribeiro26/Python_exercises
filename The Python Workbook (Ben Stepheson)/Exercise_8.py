2aa92a3f7ef37739abad506ea3b1dd346ffd7606
Alexandre Ribeiro <53876467+Alexandre-Ribeiro26@users.noreply.github.com>
2021-08-03 00:55:34 -0300

    Create Exercise_8.py

---
+++ The Python Workbook (Ben Stepheson)/Exercise_8.py
@@ -0,0 +1,8 @@
+# Widgets and Gizmos
+
+widgets = int(input('How many widgets do you want to buy?'))
+gizmos = int(input('And gizmos?'))
+
+total_weight = (widgets * 75) + (gizmos * 112)
+
+print(f'The total weight of the order is {total_weight} grams')

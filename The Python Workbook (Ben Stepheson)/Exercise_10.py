ae3e337c8a20a632810a20c32471742fc0e6d9a4
Alexandre Ribeiro <53876467+Alexandre-Ribeiro26@users.noreply.github.com>
2021-08-06 00:35:52 -0300

    Create Exercise_10.py

---
+++ The Python Workbook (Ben Stepheson)/Exercise_10.py
@@ -0,0 +1,14 @@
+# Arithmetic
+
+import math
+
+a = int(input('Give me an integer number'))
+b = int(input('Give me another integer number'))
+
+print(a + b)
+print(b - a)
+print(a * b)
+print(a / b)
+print(a % b)
+print(math.log(a,10))
+print(a ** b)
